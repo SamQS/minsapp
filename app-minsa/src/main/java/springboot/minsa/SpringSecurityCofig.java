138f7f84889f5109f056f0488470b94242e884d9
Samuel <diegodsqs2@gmail.com>
2022-11-12 08:56:47 -0500

    changes

--- app-minsa/src/main/java/springboot/minsa/SpringSecurityCofig.java
+++ app-minsa/src/main/java/springboot/minsa/SpringSecurityCofig.java
@@ -1,54 +1,50 @@
-/*package springboot.minsa;
 
-import org.springframework.beans.factory.annotation.Autowired;
+package springboot.minsa;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+/*import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
+*/
 public class SpringSecurityCofig {
-
+/*
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/imagenes/**", "/status/**").permitAll() //Publico todos los archivos como js/css/imagenes
-				.antMatchers("/login*").permitAll()
-				.antMatchers("/admin*").hasAnyRole("ADMIN")
-				.antMatchers("/userInterno*").hasAnyRole("USER_INTERNO")
-				.antMatchers("/userExterno*").hasAnyRole("USER_EXTERNO")
-				.antMatchers("/userAsistencial*").hasAnyRole("USER_ASISTENCIAL")
-					.anyRequest().authenticated()
-					.and()
-				.formLogin().
-					loginPage("/login").permitAll()
-					.defaultSuccessUrl("/menu")
-					.failureUrl("/login?error=true")
-	                .and()
-					.logout()
-					.permitAll()
-	                .logoutSuccessUrl("/login?logout");
-
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/imagenes/**").permitAll().antMatchers("/home/**")
+				.hasAnyRole("ADMIN").antMatchers("/listar/**")
+				.hasAnyRole("ADMIN").antMatchers("/form/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/menuAsistencial/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/listarServicios/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/registrarServicio/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/verServicio/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/listarCentros/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/registrarCentro/**")
+				.hasAnyRole("USER_ADMINISTRATIVO_EXTERNO").antMatchers("/ver/**")
+				.hasAnyRole("ADMIN").anyRequest().authenticated().and()
+				.formLogin().loginPage("/login")
+				.permitAll().and().logout()
+				.permitAll();
 		return http.build();
 	}
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception { 
- 
-    	PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-
-    	builder.inMemoryAuthentication()
-    	.withUser(users.username("47445223").password("5555").roles("USER_EXTERNO"))
-    	.withUser(users.username("01010101").password("1234").roles("USER_INTERNO"))
-    	.withUser(users.username("02020202").password("1234").roles("USER_ASISTENCIAL"))
-    	.withUser(users.username("80808080").password("1234").roles("ADMIN"));
-    }
-
-}*/
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withUsername("47445223").password(passwordEncoder().encode("alexquispe")).roles("USER_ASISTENCIAL").build());
+		manager.createUser(
+				User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build());
+		manager.createUser(
+				User.withUsername("11110000").password(passwordEncoder().encode("fernando")).roles("USER_ADMINISTRATIVO_EXTERNO").build());
+		
+		return manager;
+	}
+*/
+}
